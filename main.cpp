368d976bb6d9280169071217f0acd2d536efc318
eldhom <wass95@hotmail.se>
2013-03-20 13:20:50 +0100

    l

--- main.cpp
+++ main.cpp
@@ -2,11 +2,18 @@
 
 using namespace sf;
 
+const int               WINDOW_WIDTH = 800;
+const int               WINDOW_HEIGHT = 600;
+const int               TITLE_WIDTH = 100;
+const int               TITLE_HEIGHT = 20;
+const Vector2f          TITLE_POSITION(WINDOW_WIDTH/2 - TITLE_WIDTH, 50);
 
 int main()
 {
-    RenderWindow                App(VideoMode(800, 600), "Cong");
+    RenderWindow                App(VideoMode(WIDTH, HEIGHT), "Cong");
     Event                       event;
+    RectangleShape              title(Vector2f(100,20));
+    title.setPosition(
 
     while(App.isOpen())
     {
@@ -16,5 +23,9 @@ int main()
                 App.close();
             }
         }
+
+        App.clear();
+        App.draw(title);
+        App.display();
     }
 }
