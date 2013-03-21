b942ae7f90e326264c93f73023c51cee95523e53
eldhom <wass95@hotmail.se>
2013-03-21 14:51:03 +0100

    nästan en meny

---
+++ Button.hpp
@@ -0,0 +1,24 @@
+
+
+class Button {
+    
+    Font                    font; 
+    Text                    text; 
+    string                  textString;
+    int                     textiCharacterSize;
+    Color                   textColor;
+    Vector2f                textPosition();
+    RectangleShape          box;
+    Color                   boxColor;
+    Vector2f                boxSize;
+    Vector2f                boxPosition;
+
+public:
+    void                    setButtonPosition();
+    void                    setTextColor();
+    void                    setTextFont();
+    void                    setTextString();
+    void                    setTextSize();
+    void                    setBoxSize();
+    void                    setBoxColor();
+};
