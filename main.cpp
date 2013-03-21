58120b2e59015e6c2e6e3c8fb8097d35d03ecdb2
eldhom <wass95@hotmail.se>
2013-03-21 09:10:15 +0100

    Play knapp

--- main.cpp
+++ main.cpp
@@ -2,30 +2,40 @@
 using namespace std;
 using namespace sf;
 
-const int               WINDOW_WIDTH = 800;
-const int               WINDOW_HEIGHT = 600;
+const int                   WINDOW_WIDTH = 800;
+const int                   WINDOW_HEIGHT = 600;
 
-const string            TITLE_FONT_NAME = "Feisar.ttf";
-const string            TITLE_STRING = "Cong";
-const Vector2f          TITLE_SIZE(200, 50);
-const Vector2f          TITLE_POSITION((WINDOW_WIDTH-TITLE_SIZE.x)/2, 50);
+const string                FONT_FILE_NAME = "Feisar.ttf";
+const string                TITLE_STRING = "CONG";
+const Vector2f              TITLE_BOX_SIZE(200, 50);
+const Vector2f              TITLE_POSITION((WINDOW_WIDTH-TITLE_BOX_SIZE.x)/2, 50);
+const string                PLAY_BUTTON_STRING = "PLAY!";
+const Vector2f              PLAY_BUTTON_SIZE(200, 50);
+const Vector2f              PLAY_BUTTON_POSITION((WINDOW_WIDTH-TITLE_BOX_SIZE.x)/2, 200);
 
 
 int main()
 {
-    Font                TITLE_FONT;
-    Text                TITLE_TEXT;
-    RectangleShape      TITLE_BOX(TITLE_SIZE);
-    RenderWindow        App(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Cong");
-    Event               event;
+    Font                    FONT;
+    Text                    TITLE_TEXT;
+    RectangleShape          TITLE_BOX(TITLE_BOX_SIZE);
+    Text                    PLAY_BUTTON_TEXT;
+    RectangleShape          PLAY_BUTTON_BOX(PLAY_BUTTON_SIZE);
+    RenderWindow            App(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "CONG");
+    Event                   event;
 
-    TITLE_FONT.loadFromFile(TITLE_FONT_NAME);
+    FONT.loadFromFile(FONT_FILE_NAME);
     TITLE_TEXT.setString(TITLE_STRING);
-    TITLE_TEXT.setFont(TITLE_FONT);
+    TITLE_TEXT.setFont(FONT);
     TITLE_TEXT.setColor(Color(0,0,0));
     TITLE_TEXT.setCharacterSize(40);
     TITLE_TEXT.setPosition(Vector2f(320, 50));
     TITLE_BOX.setPosition(TITLE_POSITION);
+    PLAY_BUTTON_TEXT.setString(PLAY_BUTTON_STRING);
+    PLAY_BUTTON_TEXT.setFont(FONT);
+    PLAY_BUTTON_TEXT.setColor(Color(0,0,0));
+    PLAY_BUTTON_TEXT.setPosition(Vector2f(320, 200));
+    PLAY_BUTTON_BOX.setPosition(PLAY_BUTTON_POSITION);
 
     while(App.isOpen())
     {
